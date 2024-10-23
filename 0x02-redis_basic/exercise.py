d123a8cdc380fc38e0efda2b706bfc2312a8760e
wandilemawelela <wandile1997@gmail.com>
2024-10-23 20:58:18 +0000

    [ADD] Completd task 3: use rpush to append the input arguments. Remember that Redis can only store strings, bytes and numbers. Therefore, we can simply use str(args) to normalize. We can ignore potential kwargs for now.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,12 +13,30 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+
+        # Store input arguments
+        self._redis.rpush(input_key, str(args))
+
+        # Execute the wrapped function and store the output
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(output))
+
+        return output
+    return wrapper
+
+
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
@@ -61,5 +79,11 @@ if __name__ == "__main__":
 
     # Check if count_calls is working
     print(f"store method called {cache._redis.get('Cache.store_calls').decode('utf-8')} times")
+
+    # Check call history
+    inputs = cache._redis.lrange('Cache.store:inputs', 0, -1)
+    outputs = cache._redis.lrange('Cache.store:outputs', 0, -1)
+    print(f"store method inputs: {inputs}")
+    print(f"store method outputs: {outputs}")
 
     print("All tests passed.")
